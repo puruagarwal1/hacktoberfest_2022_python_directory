49b6d9fb37b41b8c54f20bce0a3701cf7f857e4b
Tr0nML <93488188+Tr0nML@users.noreply.github.com>
2022-10-22 18:22:09 +0530

    Optimised Code
    N/A

--- python/7.py
+++ python/7.py
@@ -1,7 +1,7 @@
-def alphacount(s):
+def alphacount(str):
     mydict={}
-    for item in s:
-        mydict.update({item:s.count(item)})
+    for item in str:
+        mydict.update({item:str.count(item)})
     return mydict
 str = input("Enter a string : ")
 print(alphacount(str))
