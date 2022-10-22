9f966def8bd79691cf7fe5582c570d29135a7db3
Tr0nML <93488188+Tr0nML@users.noreply.github.com>
2022-10-22 17:25:54 +0530

    CC program with more utility
    ->Added flexibility by better logic and space evaluation
    ->Made more user-friendly

--- python/4.py
+++ python/4.py
@@ -1,5 +1,18 @@
-cc = input("Enter your Credit Card Number: ")
-l = len(cc);d4 = ""
-for i in range(-4,0):
-    d4 +=cc[i]
-print("*"*(l-4)+d4)
+cci = input("Enter your Credit Card Number : ") 
+l = len(cci)
+d4=""
+cc = ""
+if l>16:
+    for j in range(0,l):
+        if cci[j]!=" ":
+            cc +=cci[j]
+else :
+    cc=cci
+
+len=len(cc)
+if len==16:
+    for i in range(-4,0):
+        d4 +=cc[i]
+    print("Your CC is "+ "**** "*3+d4)
+else:
+    print("Enter Valid CC number")
